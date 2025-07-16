59f21308da540dba95ba3020c1c5b73cbd22e77d
Nandini Mandli <mandlinandini.1124@gmail.com>
2025-07-16 15:27:36 +0530

    Update eks.tf

--- EKS/eks.tf
+++ EKS/eks.tf
@@ -1,27 +1,48 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "12.2.0"
+  version = "19.15.1"
 
   cluster_name                   = local.name
   cluster_endpoint_public_access = true
 
-  vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
+  cluster_addons = {
+    coredns = {
+      most_recent = true
+    }
+    kube-proxy = {
+      most_recent = true
+    }
+    vpc-cni = {
+      most_recent = true
+    }
+  }
 
-  worker_groups_launch_template = [
-    {
-      name                    = "amc-cluster-wg"
-      instance_type           = "t3.large"
-      asg_desired_capacity    = 1
-      asg_min_size            = 1
-      asg_max_size            = 2
-      spot_price              = "auto"
-      override_instance_types = ["t3.large"]
-      additional_tags = {
+  vpc_id                   = module.vpc.vpc_id
+  subnet_ids               = module.vpc.private_subnets
+  # control_plane_subnet_ids = module.vpc.intra_subnets
+
+  # # EKS Managed Node Group(s)
+  # eks_managed_node_group_defaults = {
+  #   ami_type       = "AL2_x86_64"
+  #   instance_types = ["m5.large"]
+
+  #   attach_cluster_primary_security_group = true
+  # }
+
+  eks_managed_node_groups = {
+    amc-cluster-wg = {
+      min_size     = 1
+      max_size     = 2
+      desired_size = 1
+
+      instance_types = ["t3.large"]
+      capacity_type  = "SPOT"
+
+      tags = {
         ExtraTag = "helloworld"
       }
     }
-  ]
+  }
 
   tags = local.tags
 }
