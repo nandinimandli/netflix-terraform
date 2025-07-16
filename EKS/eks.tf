10cb3a467af2bb088c4957fbd463673598a43aeb
Nandini Mandli <mandlinandini.1124@gmail.com>
2025-07-16 15:00:22 +0530

    Update eks.tf

--- EKS/eks.tf
+++ EKS/eks.tf
@@ -1,6 +1,6 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "3.19.0"
+  version = "18.30.2"
 
   cluster_name                   = local.name
   cluster_endpoint_public_access = true
