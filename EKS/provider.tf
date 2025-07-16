a9ec2a8c240c25cefd25d2063e22b0e99cb01317
Nandini Mandli <mandlinandini.1124@gmail.com>
2025-07-16 14:53:41 +0530

    Update provider.tf

--- EKS/provider.tf
+++ EKS/provider.tf
@@ -14,12 +14,12 @@ locals {
 
 
 terraform {
-  required_version = ">= 1.12.0"
+  required_version = ">= 1.3.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.33.0"  # known stable version
+      version = "~> 4.67.0"
     }
   }
 }
